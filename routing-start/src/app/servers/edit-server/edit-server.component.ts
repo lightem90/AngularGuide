e3330bb31cfed995f6a95d34d61892f05b22f521
Matteo <matteo.liera@gmail.com>
2018-12-30 11:34:33 +0100

    redirection and route outsourcing (section 11 wip)

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,6 +12,7 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(
     private serversService: ServersService,
@@ -24,7 +25,10 @@ export class EditServerComponent implements OnInit {
     console.log(this.route.snapshot.fragment);
 
     //registrations
-    this.route.queryParams.subscribe();
+    this.route.queryParams.subscribe(
+      (params: Params) => {
+        this.allowEdit = params['allowEdit'] === '1';
+      });
     this.route.fragment.subscribe();
 
     this.server = this.serversService.getServer(1);
